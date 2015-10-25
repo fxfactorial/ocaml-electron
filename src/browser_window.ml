9ffaaed826ab0aacf40e9ee961c1c5afb5455bb6
Edgar Aroutiounian <edgar.factorial@gmail.com>
2015-10-24 22:43:22 -0700

    Add many methods for session and download_item

--- src/browser_window.ml
+++ src/browser_window.ml
@@ -16,6 +16,26 @@ class type download_item = object
     Js.js_string Js.t ->
     unit Js.meth
 
+  method pause : unit -> unit Js.meth
+
+  method resume : unit -> unit Js.meth
+
+  method cancel : unit -> unit Js.meth
+
+  method getUrl : unit -> Js.js_string Js.t Js.meth
+
+  method getMimeType : unit -> Js.js_string Js.t Js.meth
+
+  method hasUserGesture : unit -> bool Js.meth
+
+  method getFilename : unit -> Js.js_string Js.t Js.meth
+
+  method getTotalBytes : unit -> int Js.meth
+
+  method getReceivedBytes : unit -> int Js.meth
+
+  method getContentDisposition : unit -> Js.js_string Js.t Js.meth
+
 end
 
 class type session = object
@@ -25,6 +45,22 @@ class type session = object
     (* TODO Find way to change 'a into web_contents *)
     (Nodejs.Events.event Js.t -> download_item Js.t -> 'a Js.t -> unit) Js.callback ->
     unit Js.meth
+
+  method clearCache : (unit -> unit) Js.callback -> unit Js.meth
+
+  (** Sets download saving directory. By default, the download
+      directory will be the Downloads under the respective app
+      folder. *)
+  method setDownloadPath : Js.js_string Js.t -> unit Js.meth
+
+  method enableNetworkEmulation :
+    <offline : bool;
+     latency: int;
+     downloadThroughput : int;
+     uploadThroughput : int> Js.t ->
+    unit Js.meth
+
+  method disableNetworkEmulation : unit -> unit Js.meth
 
 end
 
@@ -38,6 +74,74 @@ class type web_contents = object
   method getUrl : unit -> Js.js_string Js.t Js.meth
 
   method session : session Js.t Js.readonly_prop
+
+  method getTitle : unit -> Js.js_string Js.t Js.meth
+
+  method isLoading : unit -> bool Js.meth
+
+  method isWaitingForResponse : unit -> bool Js.meth
+
+  method stop : unit -> unit Js.meth
+
+  method reload : unit -> unit Js.meth
+
+  method reloadIgnoringCache : unit -> unit Js.meth
+
+  method canGoBack : unit -> bool Js.meth
+
+  method canGoForard : unit -> bool Js.meth
+
+  method canGoToOffset : int -> bool Js.meth
+
+  method clearHistory : unit -> unit Js.meth
+
+  method goBack : unit -> unit Js.meth
+
+  method goForward : unit -> unit Js.meth
+
+  method goToIndex : int -> unit Js.meth
+
+  method goToOffset : int -> unit Js.meth
+
+  method isCrashed : unit -> bool Js.meth
+
+  method setUserAgent : Js.js_string Js.t -> unit Js.meth
+
+  method getUserAgent : unit -> Js.js_string Js.t Js.meth
+
+  method insertCSS : Js.js_string Js.t -> unit Js.meth
+
+  method executeJavaScript : Js.js_string Js.t -> unit Js.meth
+
+  method executeJavaScript_withBool : Js.js_string Js.t -> bool -> unit Js.meth
+
+  method setAudioMuted : bool -> unit Js.meth
+
+  method isAudioMuted : unit -> bool Js.meth
+
+  method undo : unit -> unit Js.meth
+
+  method redo : unit -> unit Js.meth
+
+  method cut : unit -> unit Js.meth
+
+  method copy : unit -> unit Js.meth
+
+  method paste : unit -> unit Js.meth
+
+  method pasteAndMatchStyle : unit -> unit Js.meth
+
+  method delete : unit -> unit Js.meth
+
+  method selectAll : unit -> unit Js.meth
+
+  method unselect : unit -> unit Js.meth
+
+  method replace : Js.js_string Js.t -> unit Js.meth
+
+  method replaceMisspelling : Js.js_string Js.t -> unit Js.meth
+
+  method print : <silent : bool; printBackground : bool> Js.t -> unit Js.meth
 
 end
 
