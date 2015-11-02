eda704ef0e4d1644996d2d24352d03f1fe28aed4
Edgar Aroutiounian <edgar.factorial@gmail.com>
2015-11-02 08:32:45 -0800

    Implement Clipboard and Crash Reporter

--- src/electron.ml
+++ src/electron.ml
@@ -3,14 +3,115 @@
 
 open Nodejs
 
-module Clipboard = struct
-end
-
-module Crash_reporter = struct
-end
 
 module Native_image = struct
 end
+
+(** The clipboard module provides methods to perform copy and paste
+    operations.  *)
+module Clipboard = struct
+
+  class clipboard = object
+
+    val raw_js = require_module "clipboard"
+
+    (** Returns the content in the clipboard as plain text. *)
+    method read_text typ : string =
+      match typ with
+      | None -> m raw_js "readText" [||]
+      | Some t -> m raw_js "readText" [|i (Js.string t)|]
+
+    (** Writes the text into the clipboard as plain text. *)
+    method write_text ?typ text : unit =
+      match typ with
+      | None -> m raw_js "writeText" [|i (Js.string text)|]
+      | Some t -> m raw_js "writeText" [|i (Js.string text); i (Js.string t)|]
+
+    (** Returns the content in the clipboard as markup. *)
+    method read_html typ : string =
+      (match typ with
+      | None -> m raw_js "readHtml" [||]
+      | Some t -> m raw_js "readHtml" [|i (Js.string t)|])
+      |> Js.to_string
+
+    (** Writes markup to the clipboard. *)
+    method write_html typ markup : unit =
+      match typ with
+      | None -> m raw_js "writeHtml" [|i (Js.string markup)|]
+      | Some t -> m raw_js "writeHtml" [|i (Js.string t);
+                                         i (Js.string markup)|]
+
+    (* method read_image *)
+    (* method write_image *)
+    method clear typ : unit =
+      match typ with
+      | None -> m raw_js "clear" [||]
+      | Some t -> m raw_js "clear" [|i (Js.string t)|]
+
+    method available_formats typ : unit =
+      match typ with
+      | None -> m raw_js "availableFormats" [||]
+      | Some t -> m raw_js "availableFormats" [|i (Js.string t)|]
+
+    (* method has *)
+    (* method read *)
+    (* method write *)
+
+  end
+
+end
+
+module Crash_reporter = struct
+
+  type opts = {product_name : string;
+               company_name : string;
+               submit_url : string;
+               auto_submit : bool ;
+               ignore_system_crash_handler : bool;
+               (** An object you can define that will be sent along
+                   with the report, only string properties are sent
+                   correctly and nested objects are not supported. *)
+               extra : Js.Unsafe.any; }
+
+  let of_opts
+      {product_name = p_name;
+       company_name = c_name;
+       submit_url = s_uri;
+       auto_submit = a_submit;
+       ignore_system_crash_handler = ig;
+       extra = e} =
+    obj_of_alist [("productName", i (Js.string p_name));
+                  ("companyName", i (Js.string c_name));
+                  ("submitUrl", i (Js.string s_uri));
+                  ("autoSubmit", i (Js.bool a_submit));
+                  ("ignoreSystemCrashHandler", i (Js.bool ig));
+                  ("extra", i e)]
+
+  let default_opts =
+    obj_of_alist [("productName", i (Js.string "Electron"));
+                  ("companyName", i (Js.string "GitHub, Inc."));
+                  ("submitUrl", i (Js.string "http://54.249.141.255:1127/post"));
+                  ("autoSubmit", i (Js.bool true));
+                  ("ignoreSystemCrashHandler", i (Js.bool false));
+                  ("extra", i (obj_of_alist []))]
+
+  class crash_reporter = object
+
+    val raw_js = require_module "crash-reporter"
+
+    method start opts : unit =
+      match opts with
+      | None -> m raw_js "start" [|i default_opts|]
+      | Some o -> m raw_js "start" [|i (of_opts o)|]
+
+    (* method get_last_crash_report =  *)
+
+    (* method get_uploaded_reports =  *)
+
+  end
+
+end
+
 
 module Screen = struct
 
@@ -34,7 +135,7 @@ module Screen = struct
       (f : (Events.event ->
             Js.Unsafe.any ->
             string Js.js_array-> unit)) : unit =
-      m raw_js "on" [|i (Js.string "display-removed"); i f|]
+      m raw_js "on" [|i (Js.string "display-metrics-changed"); i f|]
 
     method get_cursor_screen_point : <x : int; y : int> =
       let rect = m raw_js "getCursorScreenPoint" [||] in
